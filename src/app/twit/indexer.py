correct length computation

src/app/twit/indexer.py
@@ -126,7 +126,7 @@ class IndexManager(object):
                     if diff_rate>=0:
                        normalized_diff=len(str(diff_rate))*int(str(diff_rate)[:1])
                     else:
-                       normalized_diff=-len(str(diff_rate[1:]))*int(str(diff_rate)[1])
+                       normalized_diff=-len(str(diff_rate)[1:])*int(str(diff_rate)[1])
                     #interact=2*(friends_count*followers_count)/(followers_count+friends_count)
                     #if diff_rate!=0:
                     score_connection=np.log2(followers_count)*(normalized_diff)